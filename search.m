b1b585126c6dfcf4895f98eac5dc260270e2462f
Rok Grmek <grmekrok@gmail.com>
2017-04-27 23:10:09 +0200

    Added 'min_cos' argument.

--- search.m
+++ search.m
@@ -1,16 +1,17 @@
 args = argv();
 base_path = args{1};
+min_cos = str2num(args{2});
 
 load([base_path, 'generated_search_data.mat']);
 
 q = zeros(length(unique_words), 1);
-for i = 2:length(args)
+for i = 3:length(args)
 	q = q | ismember(unique_words, lower(args{i}(isalnum(args{i}))));
 end
 
 q2 = q' * U * inv(S);
 cos = (V * q2') ./ (sqrt(sum(q2.^2)) * sqrt(sum(V.^2, 2)));
-relevant_docs = sortrows([(1:number_of_docs)', cos](cos > 0.8, :), -2);
+relevant_docs = sortrows([(1:number_of_docs)', cos](cos > min_cos, :), -2);
 doc_names = file_names(relevant_docs(:, 1));
 
 result = '';
