4ee7d710c251aec2c309dccf2c759ca26c5dbfb1
Rok Grmek <grmekrok@gmail.com>
2017-04-27 20:46:00 +0200

    Updated 'search.m'.

--- search.m
+++ search.m
@@ -1,32 +1,20 @@
-%function result = search(search_str)
-  
-  search_arguments = argv();%dobi vse argumente
-  stBesed = str2num(search_arguments{1});%prvi argument je dolzina iskalnega niza
-  search_str=[]; 
-  for r = 2:1+stBesed;
-	search_str = strcat(search_str,cstrcat(' ',search_arguments{r}));
-  pot = search_arguments{2+stBesed};%zadnji argument je pot do podatkov .mat
-  end
-  search_str;
-  pot;
-  
-  
-  load(pot);
-  
-  search_words = strsplit(search_str);
-  q = zeros(length(unique_words), 1);
-  for i=1:length(search_words)
-    q = q | ismember(unique_words, search_words{i});
-  end
-  
-  q2 = q' * U * inv(S);
-  cos = (V * q2') ./ (sqrt(sum(q2.^2)) * sqrt(sum(V.^2, 2)));
-  relevant_docs = sortrows([(1:number_of_docs)', cos](cos > 0.8, :), -2);
-  doc_names = file_names(relevant_docs(:, 1));
-  cos_values = relevant_docs(:, 2);
-  
-  CellAr = {};
-  CellAr{1}=doc_names 
-  CellAr{2}=cos_values
-  
-%end
+args = argv();
+base_path = args{1};
+
+load([base_path, 'generated_search_data.mat']);
+
+q = zeros(length(unique_words), 1);
+for i = 2:length(args)
+	q = q | ismember(unique_words, args{i});
+end
+
+q2 = q' * U * inv(S);
+cos = (V * q2') ./ (sqrt(sum(q2.^2)) * sqrt(sum(V.^2, 2)));
+relevant_docs = sortrows([(1:number_of_docs)', cos](cos > 0.8, :), -2);
+doc_names = file_names(relevant_docs(:, 1));
+
+result = '';
+for i = 1:length(relevant_docs)
+	result = [result; num2str(relevant_docs(i, 2)), "\t", doc_names{i}];
+end
+result
