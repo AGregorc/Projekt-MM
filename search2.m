f45bb96383e4d1fad81cc18ed404d798f5df257a
domengasperlin <domen.gasperlin@gmail.com>
2017-04-27 19:29:21 +0200

    Update search2.m

--- search2.m
+++ search2.m
@@ -1,6 +1,17 @@
-function [doc_names, cos_values] = search2(search_str)
+%function result = search(search_str)
   
-  load('generated_search_data.mat');
+  search_arguments = argv();%dobi vse argumente
+  stBesed = str2num(search_arguments{1});%prvi argument je dolzina iskalnega niza
+  search_str=[]; 
+  for r = 2:1+stBesed;
+	search_str = strcat(search_str,cstrcat(' ',search_arguments{r}));
+  pot = search_arguments{2+stBesed};%zadnji argument je pot do podatkov .mat
+  end
+  search_str;
+  pot;
+  
+  
+  load(pot);
   
   search_words = strsplit(search_str);
   q = zeros(length(unique_words), 1);
@@ -13,5 +24,7 @@ function [doc_names, cos_values] = search2(search_str)
   relevant_docs = sortrows([(1:number_of_docs)', cos](cos > 0.8, :), -2);
   doc_names = file_names(relevant_docs(:, 1));
   cos_values = relevant_docs(:, 2);
+  suc="oja"
+  [doc_names, cos_values]
   
-end
+%end
